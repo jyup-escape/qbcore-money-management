686b22259fe6bc3ac6047db86ef6214305bb97d1
jyup-akarilab <jyup.ect@gmail.com>
2025-01-12 22:52:27 +0900

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'Display balances of all users on the server using QBcore'
+version '1.0.0'
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'qb-core'
+}
